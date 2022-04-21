cd506125f46343fdff5844f12a62dde91b35e9cf
Jaap Elst <jaapelst@gmail.com>
2022-04-21 08:23:37 +0200

    changed assets dir

--- routes/web.go
+++ routes/web.go
@@ -3,7 +3,7 @@ package routes
 import "project_name/controllers"
 
 func webRoutes() {
-	a.StaticDir("/static", "./static")
+	a.StaticDir("/assets", "./static")
 
 	a.GET("/", controllers.HomeController, "home")
 }
